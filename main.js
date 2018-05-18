057de90771fea49b39fd591aa7c44f9a8632b993
Liza Geshiktor <liza@Lizas-MacBook-Air.local>
2018-05-19 00:26:18 +0300

    pinning qoute cards

--- main.js
+++ main.js
@@ -39,11 +39,13 @@ function classTogglr(target,class1,class2)
 
 function renderQuote(response) {
   if (response.charAt(0) == "C") {
+    $('#current_buttons').style.display = "none";
     $('.quote').classList.add('errMsg');
     $('.quote').innerHTML = response;
     $('.author').innerHTML = ""; 
   }
   else {
+    $('#current_buttons').style.display = "block";
     $('.quote').classList.remove('errMsg');
     const contentsObj = JSON.parse(response);
     $('.quote').innerHTML = contentsObj.quoteText;
@@ -64,7 +66,60 @@ function getQuote() {
   })
 }
 
-$('#reload_button').addEventListener("click", (event) => {
+function Quote(text,author) {
+  this.text = text;
+  this.author = author;
+}
+
+
+
+function renderPinnedTitle() {
+  let title = document.createElement('div');
+  title.classList.add('pinned_quotes_title');
+  title.innerHTML = "My pinned quotes:";
+  $('.quotes_wrapper').appendChild(title);
+}
+
+function pinCurrentQuote() {
+  let p_quote = document.createElement('div');
+  $('.pinned_quotes_title').insertAdjacentElement('afterend', p_quote);
+  //p_quote = $('.quotes_wrapper').insertAfter(p_quote, $('.pinned_quotes_title'));
+  p_quote.classList.add('single_quote_wrapper');
+  p_quote.innerHTML = 
+    `<div class="pinned_quote_bground">
+      <div class="quote">${pinnedQuotes[pinnedQuotes.length-1].text}</div>
+      <div class="author">${pinnedQuotes[pinnedQuotes.length-1].author}</div>
+    </div>
+    <div class="buttons pinned_buttons" id="current_buttons">
+      <button><i class="fas fa-times"></i></button>
+      <button><i class="fas fa-share-alt fa-sm"></i></button>
+    </div>
+    `;
+  //$('.quotes_wrapper').appendFirstChild(p_quote);
+  
+}
+
+function renderPinnedQuotes() {
+  if(pinnedQuotes.length === 0) {
+    return;
+  }
+  if(pinnedQuotes.length === 1) {
+    renderPinnedTitle();
+  }
+  pinCurrentQuote();
+}
+
+function pinQuote() {
+  let newQuote = new Quote($('.quote').innerHTML, $('.author').innerHTML);
+  pinnedQuotes.push(newQuote);
+  renderPinnedQuotes();
+}
+
+$('#pin_quote').addEventListener("click", (event) => {
+  pinQuote();
+})
+
+$('#load_quote').addEventListener("click", (event) => {
   getQuote();
 })
 
