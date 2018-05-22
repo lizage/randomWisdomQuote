90dd869b342a58377fb4213a4954c808098ed722
Liza Geshiktor <liza@Lizas-MacBook-Air.local>
2018-05-22 15:44:57 +0300

    Pinning qoutes working

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
 const $ = (s) => document.querySelector(s);
 const $$ = (s) => document.querySelectorAll(s);
 
+
+// boilerplate for creating DOM elements
+function newNode(node, class_arr, id, parent, html) {
+    const new_node = document.createElement(node);
+    for (let className of class_arr) {
+      new_node.classList.add(className);
+    }
+    new_node.setAttribute('id', id);
+    if (parent) {
+      parent.appendChild(new_node);
+    }
+    if (html) {
+      new_node.innerHTML = html;
+    }
+    return new_node;
+  }
+
 function buildMandala() {
-  
-  const mandala_top_div = document.createElement('div');
-  mandala_top_div.classList.add('mandala');
-  mandala_top_div.classList.add('mandala_top');
-  mandala_top_div.classList.add('spinner_show');
+  const mandala_top_div = newNode('div', ['mandala', 'mandala_top', 'spinner_show']);
   $('.menu_wrapper').insertAdjacentElement('afterend', mandala_top_div);
-  
   for(let i=0; i<7; i++) {
-    let m_div = document.createElement('div'); 
-    mandala_top_div.appendChild(m_div);
-    
+    const m_div = newNode('div', [], '', mandala_top_div);
     for(let j=1; j<4; j++) {
-      let m_inner = document.createElement('div');
-      m_inner.classList.add('m_inner' + j);
-      m_div.appendChild(m_inner);
+      const m_inner = newNode('div', ['m_inner' + j], '', m_div);
     }
   }
 }
@@ -36,12 +44,12 @@ function classTogglr(target,class1,class2)
 function toggleWaitingMode() {
   classTogglr('#current_quote','quote_show','quote_hide');
   classTogglr('.mandala_top','spinner_hide','spinner_show');
-  
 }
 
+// id starts with '_' bc DOM object id can't start with a digit
 function getUniqueId(quoteObj) {
-  let link_arr = quoteObj.quoteLink.split('/'); 
-  return quoteObj.quoteLink.split('/')[link_arr.length-2];
+  const link_arr = quoteObj.quoteLink.split('/'); 
+  return '_' + link_arr[link_arr.length-2];
 }
 
 // Quote object constructor
@@ -54,22 +62,22 @@ function Quote(text,author,id) {
 function handleResponse(response) {
   // Error case
   if (response.charAt(0) == "C") {
+    $('#current_quote > .quote_bground > .quote').innerHTML = response;
     $('#current_quote > .buttons').style.display = "none";
     $('#current_quote > .quote_bground > .quote').classList.add('errMsg');
-    $('#current_quote > .quote_bground > .quote').innerHTML = response;
-    $('#current_quote > .quote_bground > .author').innerHTML = ""; 
   }
   // Response OK case
   else {
-    $('#current_quote > .buttons').style.display = "block";
-    $('#current_quote > .quote_bground > .quote').classList.remove('errMsg');
     const quoteObj = JSON.parse(response);
     currentQuote = new Quote(quoteObj.quoteText, quoteObj.quoteAuthor, getUniqueId(quoteObj));
     $('#current_quote > .quote_bground > .quote').innerHTML = currentQuote.text;
     $('#current_quote > .quote_bground > .author').innerHTML = currentQuote.author;
+    $('#current_quote > .buttons').style.display = "block";
+    $('#current_quote > .quote_bground > .quote').classList.remove('errMsg');
   }
 }
 
+// bring random quote from API
 function getQuote() {
   const key = randomFixedInteger(6);
   const proxyurl = "https://cors-anywhere.herokuapp.com/";
@@ -83,37 +91,35 @@ function getQuote() {
 }
 
 function renderPinnedTitle() {
-  let title = document.createElement('div');
-  title.classList.add('pinned_quotes_title');
-  title.innerHTML = "My saved quotes:";
-  $('.quotes_wrapper').appendChild(title);
+  const title = newNode('div', ['pinned_quotes_title'], '', $('.quotes_wrapper'), 'My saved quotes:');
 }
 
 function pinCurrentQuote() {
+  // push to array
   pinnedQuotes.push(currentQuote);
-  let quote_div = document.createElement('div');
-  quote_div.classList.add('single_quote_wrapper');
-  quote_div.classList.add('pinned_add');
-  quote_div.setAttribute('id', currentQuote.id);
-  quote_div.innerHTML = 
+  
+  // render on screen
+  const quote_div = newNode(
+    'div', 
+    ['single_quote_wrapper', 'pinned_add'], 
+    currentQuote.id, 
+    '', 
     `<div class="pinned_quote_bground">
-      <div class="quote">${currentQuote.text}</div>
-      <div class="author">${currentQuote.author}</div>
-    </div>
-    <div class="buttons pinned_buttons">
-      <button id="remove_${currentQuote.id}" class="remove_pin"><i class="fas fa-times"></i></button>
-      <button><i class="fas fa-share-alt fa-sm"></i></button>
-    </div>
-    `;
+        <div class="quote">${currentQuote.text}</div>
+        <div class="author">${currentQuote.author}</div>
+      </div>
+      <div class="buttons pinned_buttons">
+        <button id="remove_${currentQuote.id}" class="remove_pin"><i class="fas fa-times"></i></button>
+        <button><i class="fas fa-share-alt fa-sm"></i></button>
+      </div>
+      `);
     $('.pinned_quotes_title').insertAdjacentElement('afterend', quote_div); 
 }
 
 function setRemoveButton() {
-
-  $('.remove_pin').addEventListener("click", (event) => {
-    // get the id of quote to be removed
-    let id_arr = event.currentTarget.getAttribute('id').split('_');
-    let id = id_arr[id_arr.length-1];
+  $('.remove_pin').addEventListener("click", (e) => {
+    
+    const id = getCurrentId(e);
 
     // restore pin_quote button
     if(currentQuote.id === id) {
@@ -121,21 +127,24 @@ function setRemoveButton() {
     }
     
     // remove quote object from pinnedQuotes array
-    let = obj = pinnedQuotes.find(x => x.id === id);
-    let index = pinnedQuotes.indexOf(obj);
+    const obj = pinnedQuotes.find(x => x.id === id);
+    const index = pinnedQuotes.indexOf(obj);
     pinnedQuotes.splice(index, 1);
     
     // remove card from screen
-    let card = document.getElementById(id);
-    card.classList.remove('pinned_add');
-    card.classList.add('pinned_remove');
-    setTimeout(() => $('.quotes_wrapper').removeChild(card), 350);
+    classTogglr('#' + id,'pinned_add','pinned_remove');
+    setTimeout(() => $('.quotes_wrapper').removeChild($('#' + id)), 350);
     
     //remove title if array is empty
     if(pinnedQuotes.length === 0) {
-      $('.quotes_wrapper').removeChild($('.pinned_quotes_title'));
+      setTimeout(() => $('.quotes_wrapper').removeChild($('.pinned_quotes_title')), 300);
     }
   })
+}
+
+function getCurrentId(e) {
+  let id_arr = e.currentTarget.getAttribute('id').split('_');
+  return '_' +  id_arr[id_arr.length-1];
 }
 
 function renderPinnedQuotes() {
@@ -144,9 +153,6 @@ function renderPinnedQuotes() {
   }
   else if(pinnedQuotes.length === 0) {
     renderPinnedTitle();
-  }
-  else if(pinnedQuotes[pinnedQuotes.length-1].id === currentQuote.id) {
-    return;
   }
   pinCurrentQuote();
   setRemoveButton();
